a25cda3714e4856d1c8c865ddd7111312110fece
danman15726 <danman15726@gmail.com>
2020-03-25 21:03:15 -0500

    testing

---
+++ client/src/App.js
@@ -0,0 +1,88 @@
+
+
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Start from "./pages/Start";
+import Login from "./pages/Login"
+import SignUp from "./pages/SignUp"
+import About from "./pages/About"
+// import NavBarStart from "./components/NavBar-Start/index"
+// import Home from "./pages/Home"
+
+import store from "./store";
+import Dashboard from "./components/Dashboard";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
+import PrivateRoute from "./components/private-route/PrivateRoute";
+import NavBarStart from "./components/NavBar-Start"
+// Check for token to keep user logged in
+import Recipes from "./pages/Recipes";
+import Sign from "./components/Chat/index.js";
+import VideoPlayer from "./pages/VideoPlayer";
+import ChatRoom from "./components/Chat/Chat/chatroom.js";
+import Settings from "./pages/settings.js";
+import profileDashboard from "./pages/dashboard.js";
+
+
+
+import LiveStream from "./pages/LiveStream";
+
+import VideoSettings from "./pages/VideoSettings";
+
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+// Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Redirect to login
+    window.location.href = "./login";
+  }
+}
+
+const App = (props) => {
+
+
+console.log(props)
+
+return (
+
+<React.Fragment>
+  <NavBarStart/>
+   <Router>
+      
+      <Route exact path = "/intro" component = {Start} />
+      <Route exact path = "/" component = {LiveStream} />
+      <Route exact path = "/SignUp" component = {SignUp} />
+      <Route exact path ='/Login'  component={Login} /> 
+      <Route  exact path="/recipes" component={Recipes} />
+      <Route path = "/chat" component = {ChatRoom} />
+      <Route exact path = "/profile" component = {profileDashboard} />
+      
+       <Route exact path = "/settings" component = {Settings} />
+       <Route exact path = "/about" component = {About}/>
+      <Route path = "/streams/:username" component = {VideoPlayer} />
+      <Route exact path = "/VideoSettings" component = {VideoSettings} />
+      <Switch>
+        <PrivateRoute exact path="/dashboard" component={Dashboard} />
+      </Switch>
+   </Router>
+
+
+    
+  </React.Fragment>
+  
+)}
+
+
+export default App;
+
---
+++ client/src/components/Chat/Chat/chatroom.js
@@ -0,0 +1,154 @@
+import React, { useState, useEffect, useReducer } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
+
+import "./style.css";
+import Inline from "./inline.js";
+import "../../../pages/styles/VideoPlayer/videoplayer.css";
+
+let ENDPOINT;
+
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV !== 'production') {
+    ENDPOINT = "http://localhost:3002";
+};
+
+const socketv = io("http://localhost:3002")
+const ChatRoom = () => {
+    
+    const [name, setName] = useState("");
+    const [room, setRoom] = useState("");
+    
+    const [message, setMessage] = useState("");
+    const [messages, setMessages] = useState([]);
+
+    const [messagebroad, setMessagebroad] = useState("");
+    const [messagesbroad, setMessagesbroad] = useState([]);
+
+
+    
+    
+    // const streamRoom = queryString.parse(location.search);
+    const streamRoom = window.location.href;
+
+
+    useEffect(() => {
+        
+
+
+        async function connect () {
+            const socket = io.connect("http://localhost:3002");
+         }
+        connect();
+        
+        
+        socketv.on("join", streamRoom => {
+            console.log(streamRoom);
+
+            socketv.off('join', streamRoom);  
+        });
+
+        // setName(name);
+        // setRoom(room);
+        
+        
+
+        return () => {
+            socketv.emit("disconnect");
+            socketv.off();
+        }
+
+    },[streamRoom]);
+
+
+    
+    socketv.on("chatMessage", (massage) =>{
+        setMessagesbroad([massage])
+        
+        
+        
+        console.log(messagesbroad);
+            
+        
+       
+
+        const element = document.getElementById("chatIner");
+        element.scrollTop = element.scrollHeight;
+        
+    })
+
+    
+
+
+    const sendMessage = (event) => {
+        
+        if(message) {
+            socketv.emit("sendMessage", message)
+            setMessage("")
+            setMessages(oldValue =>
+                 [...oldValue, message]
+            )
+              console.log(messages); 
+        }
+        
+        console.log(message);
+    }
+    
+
+
+    // Auto-scrolling the div down on message
+    useEffect(() => {
+        const element = document.getElementById("chatIner");
+        element.scrollTop = element.scrollHeight;
+
+    }, [message]);
+
+    return ( 
+        <React.Fragment>
+            <div className = "outer">
+                {/* <div className = "wrapper"> */}
+                    
+                    
+
+                <div className="bg-dark sideb" style = {{backgroundColor: "#1E1E1E"}}>
+                    <div className = "chat rounded-left fixed-right">
+                        <div className = "Title" align="center"><h1>Chat</h1></div>
+                        <div className=" chatinner rounded-bottom border border-white " id = "chatIner" style = {{backgroundColor: "#FAFAFA"}}>
+                            
+                                <Inline username = {"Cartman"} message = {"Hey I'm your biggest fan!"}/>
+                                <Inline username = {"Kyle"} message = {"oh em geee, it's streamerman!"}/>
+                                <Inline username = {"Kenny"} message = {"MhmhmnngngHMMHMM"}/>
+                                <Inline username = {"Randy Marsh"} message = {"OHMYGAWLITSGORDONRAM SEY!!!"}/>
+                                <Inline username = {"Stan"} message = {"Beef Wellington"}/>
+                                <Inline username = {"Randy Marsh"} message = {"Creme Fraiche!"}/>
+                                
+
+                                {messagesbroad.map((result, i) =>(
+                                    
+                                    <Inline username = {"Stan"} message ={result}  key = {i}/>
+                                ))}
+
+
+                                {messages.map((result, i) =>(
+                                    
+                                    <Inline username = {"User"} message ={result}  key = {i}/>
+                                ))}
+
+                           
+
+
+                        </div>
+                        <input className="form-control form-control-sm" type="text" placeholder="Say Something!" value = {message}  
+                        onChange = {(event) => setMessage(event.target.value)}  onKeyPress = {event => event.key === "Enter" ? sendMessage(event.target.value) : null}></input>
+                        {/* <button type="button" className="btn " type = "submit" >Enter</button> */}
+                    </div>
+
+                </div>
+            </div>
+        </React.Fragment>   
+
+
+    )
+}
+
+export default ChatRoom;
